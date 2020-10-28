0689cbbc3a5f9b54631d6e472c87c789f625b7ae
Remi Douah <remidouah@gmail.com>
2020-10-27 21:42:08 -0500

    Voting Rights

---
+++ mystory2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace mystory2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ;");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+
+
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+
+
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ stories/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace stories
+{
+    class Program
+    {
+        static void Main(string[] args)
+ 
+        {
+
+            Console.WriteLine(GetMax(2, 10));
+
+            Console.ReadLine();
+
+
+
+
+            bool isMale = true;
+            bool isTall = false;
+
+
+            if (isMale && isTall)
+
+                {
+                    Console.WriteLine("You are a tall male");
+                } 
+            
+            else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are a short male");
+            } 
+            
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not a male but you are tall");
+
+            }
+            else
+
+            { 
+                Console.WriteLine("You are not male and not tall");
+            }
+
+            Console.ReadLine();
+        }
+
+
+        
+      static int GetMax(int num1, int num2, num3)
+        {
+
+            int result;
+
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else 
+            {
+                result = num2;
+            }
+
+            return result;
+        }
+    }
+}
